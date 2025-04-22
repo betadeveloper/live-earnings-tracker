ce259544f9e7007f6e4d2d28d416db22a91f366e
Raigardas Tautkus <raigardas@Mac.lan>
2025-04-22 22:44:31 +0300

    Add sitemap related dependencies, and scripts

---
+++ generate-sitemap.cjs
@@ -0,0 +1,22 @@
+const { SitemapStream, streamToPromise } = require('sitemap');
+const { createWriteStream } = require('fs');
+
+const links = [
+    { url: '/', changefreq: 'monthly', priority: 1.0 },
+];
+
+(async () => {
+    const stream = new SitemapStream({ hostname: 'https://yourdomain.com' });
+    const writeStream = createWriteStream('./public/sitemap.xml');
+
+    stream.pipe(writeStream);
+
+    for (const link of links) {
+        stream.write(link);
+    }
+
+    stream.end();
+
+    await streamToPromise(stream);
+    console.log('✅ Sitemap successfully generated!');
+})();
